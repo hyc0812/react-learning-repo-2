617be5bf23da317b2173a92fcb570634fa36cfb8
yongchang he <hyc0812@163.com>
2022-06-20 11:06:38 -0600

    update

--- practice-5-context/src/Header.js
+++ practice-5-context/src/Header.js
@@ -1,10 +1,14 @@
 import React, {Component} from "react"
+import UserContext from "./userContext"
 
 class Header extends Component {
+
+    static contextType = UserContext;
     render() {
+        const username = this.context;
         return (
             <header>
-                <p>Welcome, Username!</p>
+                <p>Welcome, { username }!</p>
             </header>
         )    
     }
