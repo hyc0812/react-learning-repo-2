2aa63040efcbab3706cfd2e42097a552d015939f
yongchang he <hyc0812@163.com>
2022-06-20 10:49:23 -0600

    udpate

--- README.md
+++ README.md
@@ -21,3 +21,8 @@ This is the 2ed repo for learning React and Js.
 > Render props
 
 <img src="https://github.com/hyc0812/react-learning-repo-2/blob/master/screenshot/practice_4.png" width=600>
+
+- Practice - 5
+> Context
+
+<img src="https://github.com/hyc0812/react-learning-repo-2/blob/master/screenshot/practice_4.png" width=600>
---
+++ practice-5-context/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+import React from "react"
+
+import Header from "./Header"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <main>
+                <p className="main">No new notifications, Username! 🎉</p>
+            </main>
+        </div>
+    )
+}
+
+export default App
