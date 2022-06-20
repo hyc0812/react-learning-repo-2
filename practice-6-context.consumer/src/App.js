d7a8e5fd4f9990b0b0b73e60009b6448c77417fe
yongchang he <hyc0812@163.com>
2022-06-20 11:23:30 -0600

    update

---
+++ practice-6-context.consumer/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+import React from "react"
+
+import Header from "./Header"
+import Button from "./Button"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Button />
+        </div>
+    )
+}
+
+export default App
---
+++ practice-6-context.consumer/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import ThemeContext from './themeContext';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ThemeContext.Provider value={"dark"}>
+      <App />
+    </ThemeContext.Provider>
+  </React.StrictMode>
+);
+
+
