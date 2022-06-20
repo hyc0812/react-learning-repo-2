24752efc22dadc7fb77b02ca384cc7f70e89c8e3
yongchang he <hyc0812@163.com>
2022-06-20 11:00:47 -0600

    update

--- practice-5-context/src/App.js
+++ practice-5-context/src/App.js
@@ -4,16 +4,22 @@ import './App.css';
 import React from "react"
 
 import Header from "./Header"
+import UserContext from './userContext';
 
-function App() {
-    return (
-        <div>
-            <Header />
-            <main>
-                <p className="main">No new notifications, Username! 🎉</p>
-            </main>
-        </div>
-    )
+class App extends React.Component {
+  static contextType = UserContext;
+
+    render() {
+      const username = this.context;
+      return (
+          <div>
+              <Header />
+              <main>
+                  <p className="main">No new notifications, { username }! 🎉</p>
+              </main>
+          </div>
+      )
+    }
 }
 
 export default App
--- practice-5-context/src/index.js
+++ practice-5-context/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import UserContext from './userContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <React.StrictMode>
-    <App />
+    <UserContext.Provider value={"Yongchang He"}>
+      <App />
+    </UserContext.Provider>
   </React.StrictMode>
+
 );
