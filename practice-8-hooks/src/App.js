05475b00f48b0164cd001b42409be122cf04d539
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-21 23:28:33 -0600

    update

--- practice-8-hooks/src/App.js
+++ practice-8-hooks/src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 
 import React, { useState } from "react"
 
-function App() {
-    const value = useState("Yes")
-    console.log(value)
-    
-    return (
-        <div>
-            <h1>Is state important to know? {value[0]}</h1>
-        </div>
-    )
+function App () {
+  const [ count, setCount ] = useState(0)
+
+  function increment() {
+    setCount(prevCount => prevCount + 1)
+  }
+  function decrement() {
+    setCount(prevCount => prevCount - 1)
+  }
+
+  return (
+    <div>
+      <h1>{ count }</h1>
+      <button onClick={ increment }> Change! </button>
+      <button onClick={ decrement }> Change! </button>
+    </div>
+  )
 }
+
 export default App
