010aa6efa0e249cbf806c1060e25d511b59b9da8
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-21 23:04:51 -0600

    update

---
+++ practice-8-hooks/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import React, { useState } from "react"
+
+function App() {
+    const value = useState("Yes")
+    console.log(value)
+    
+    return (
+        <div>
+            <h1>Is state important to know? Yes</h1>
+        </div>
+    )
+}
+export default App
