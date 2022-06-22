ebfa19ddf1c77dbd72dc9f97df83b100cb09c4ca
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-21 23:07:57 -0600

    update

--- practice-8-hooks/src/App.js
+++ practice-8-hooks/src/App.js
@@ -9,7 +9,7 @@ function App() {
     
     return (
         <div>
-            <h1>Is state important to know? Yes</h1>
+            <h1>Is state important to know? {value[0]}</h1>
         </div>
     )
 }
