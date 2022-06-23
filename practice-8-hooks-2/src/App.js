f2c7aac90e566ece39075943f9be33cce25c1023
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-22 21:33:54 -0600

    update

---
+++ practice-8-hooks-2/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+
+import React, {useState, useEffect} from "react"
+
+function App() {
+    const [count, setCount] = useState(0)
+    const [answer, setAnswer] = useState("Yes")
+    
+    function increment() {
+        setCount(prevCount => prevCount + 1)
+    }
+    
+    function decrement() {
+        setCount(prevCount => prevCount - 1)
+    }
+    
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
+}
+
+export default App
