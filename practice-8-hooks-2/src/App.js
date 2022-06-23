56d0060e38f1b73cda0e3fed12ab50df933bc4bd
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-22 22:02:47 -0600

    update

--- practice-8-hooks-2/src/App.js
+++ practice-8-hooks-2/src/App.js
@@ -8,14 +8,13 @@ import randomcolor from "randomcolor"
 function App() {
     const [count, setCount] = useState(0)
     const [color, setcolor] = useState("")
-    
-    function increment() {
+
+    useEffect(() => {
+      const intervalId = setInterval(() => {
         setCount(prevCount => prevCount + 1)
-    }
-    
-    function decrement() {
-        setCount(prevCount => prevCount - 1)
-    }
+      }, 1000)
+      return () => clearInterval(intervalId)
+    })
 
     useEffect(() => {
       setcolor(randomcolor())
@@ -24,8 +23,6 @@ function App() {
     return (
         <div>
             <h1 style={{ color: color }}>{count}</h1>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
         </div>
     )
 }
