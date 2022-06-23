723f269a2257984836257090b6c66341b672acec
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-22 22:17:29 -0600

    update

--- practice-9-typing-game/src/App.js
+++ practice-9-typing-game/src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Typing Game</h1>
+      <textarea />
+      <h4>Time remaining: </h4>
+      <button> START GAME</button>
+      <h1> Word Count:</h1>
 
     </div>
   );
