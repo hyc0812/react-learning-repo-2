6a128cd5deba707d20cf97683ad10a0872610b79
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-24 23:06:58 -0600

    update

--- practice-11-router/src/App.js
+++ practice-11-router/src/App.js
@@ -1,12 +1,18 @@
 
 import './App.css';
-import { Link } from "react-router-dom"
+import { Link, Routes, Route } from "react-router-dom"
+import Home from './Home';
+import About from './About';
 
 function App() {
   return (
     <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+
+      <Routes>
+      <Route exact path="/"       element={ <Home />  } />
+      <Route exact path="/about"  element={ <About /> } />
+      </Routes>
+
     </div>
   );
 }
