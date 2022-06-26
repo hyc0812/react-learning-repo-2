57ef8c16fa7054e91e71c0224dbd337b5f1b4ad3
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-25 22:17:56 -0600

    update

--- practice-12-router-useparams/src/App.js
+++ practice-12-router-useparams/src/App.js
@@ -1,11 +1,32 @@
 
 import './App.css';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+import React from "react"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import ServicesList from "./pages/services/ServicesList"
+import ServiceDetail from "./pages/services/ServiceDetail"
+
+import {Routes, Route} from "react-router-dom"
+
+function App() {    
+    return (
+        <div>
+            <Header />
+            
+            <Routes>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/services" >
+                    <ServicesList />
+                </Route>
+                <Route path="/services/:serviceId" >
+                    <ServiceDetail />
+                </Route>
+            </Routes>
+        </div>
+    )
 }
 
-export default App;
+export default App
